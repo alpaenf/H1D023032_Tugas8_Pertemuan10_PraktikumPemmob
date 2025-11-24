674aec6d87bc372e32edebc01b7e250cab49efe9
alpaenf <alfaenf_@gmail.com>
2025-11-24 18:47:25 +0700

    first commit

---
+++ lib/ui/login_page.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:tokokita/ui/registrasi_page.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  bool _isLoading = false;
+
+  final _emailTextboxController = TextEditingController();
+  final _passwordTextboxController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login Alfaen'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                _emailTextField(),
+                _passwordTextField(),
+                _buttonLogin(),
+                const SizedBox(height: 30),
+                _menuRegistrasi(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  // TextBox Email
+  Widget _emailTextField() {
+    return TextFormField(
+      decoration: const InputDecoration(labelText: "Email"),
+      keyboardType: TextInputType.emailAddress,
+      controller: _emailTextboxController,
+      validator: (value) {
+        if (value!.isEmpty) {
+          return 'Email harus diisi';
+        }
+        return null;
+      },
+    );
+  }
+
+  // TextBox Password
+  Widget _passwordTextField() {
+    return TextFormField(
+      decoration: const InputDecoration(labelText: "Password"),
+      keyboardType: TextInputType.text,
+      obscureText: true,
+      controller: _passwordTextboxController,
+      validator: (value) {
+        if (value!.isEmpty) {
+          return "Password harus diisi";
+        }
+        return null;
+      },
+    );
+  }
+
+  // Tombol Login
+  Widget _buttonLogin() {
+    return ElevatedButton(
+      child: const Text("Login"),
+      onPressed: () {
+        bool validate = _formKey.currentState!.validate();
+        if (validate) {
+          setState(() {
+            _isLoading = true;
+          });
+
+          // TODO: Tambahkan proses login (API call)
+
+          setState(() {
+            _isLoading = false;
+          });
+        }
+      },
+    );
+  }
+
+  // Menu Registrasi
+  Widget _menuRegistrasi() {
+    return Center(
+      child: InkWell(
+        child: const Text(
+          "Registrasi",
+          style: TextStyle(color: Colors.blue),
+        ),
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const RegistrasiPage()),
+          );
+        },
+      ),
+    );
+  }
+}
